eaac81ba742a3d4cf90c35e6bfcd6303831a3d0c
bigriceman0128 <112298940+ardenmonaghan@users.noreply.github.com>
2026-02-18 19:58:57 -0700

    changed location of constants

--- backend/db_helpers/db_services.py
+++ backend/db_helpers/db_services.py
@@ -10,7 +10,31 @@ from sqlalchemy import (
 # from constants import Dataset
 
 def detect_upload_type(filename: str):
+    '''
+    detect_upload_type is a function that detects the type of the file that is being uploaded.
+
+    Args: filename: str - The name of the file that is being uploaded.
+
+    Returns: 
+        str - The type of the file that is being uploaded.
+    '''
     extension = Path(filename).suffix.lower()
+
+    if extension == ".csv":
+        return "csv"
+    elif extension == ".json":
+        return "json"
+    elif extension == ".jsonl":
+        return "jsonl"
+    elif extension == ".sqlite":
+        return "sqlite"
+    elif extension == ".sql_dump":
+        return "sql_dump"
+    else:
+        return "unknown"
+
+def save_raw_file()
+
 
 if __name__ == "__main__":
     print(detect_upload_type("test.csv"))
